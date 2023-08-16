c7759ada1b08a121f13dc7647d4a68edad537c01
Joris Boschmans <joris.boschmans@gmail.com>
2023-08-16 16:16:00 +0200

    - Renamed getProperty, to get - Added the get method with a default value - Added javadoc to the init function

--- src/main/java/com/jorisboschmans/propmanager/PropManager.java
+++ src/main/java/com/jorisboschmans/propmanager/PropManager.java
@@ -1,4 +1,4 @@
-package com.jorisboschmans;
+package com.jorisboschmans.propmanager;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -12,13 +12,20 @@ import java.util.Properties;
  * if the "profile" property is not set.
  *
  * @author Joris Boschmans
- * @version 0.1
+ * @version 1.0
  * @since 2023-07-19
  */
 public class PropManager {
     private static final Properties properties = new Properties();
-
-    static {
+	
+	/**
+	 * Initializes the properties by loading them from a file.
+	 * The properties file name is determined by the "profile" system property. If the "profile"
+	 * property is not set, it defaults to "default.properties". If the specified profile name
+	 * doesn't end with ".properties", the extension is appended.
+	 * If there's any error in loading the properties file, a runtime exception is thrown.
+	 */
+	private static void init() {
         String propertiesFilename = System.getProperty("profile", "default");
 
         // Append .properties if not present
@@ -41,7 +48,19 @@ public class PropManager {
      * @param key This is the key to look up in the properties.
      * @return String This returns the property value for the given key.
      */
-    public static String getProperty(String key) {
+    public static String get(String key) {
         return properties.getProperty(key);
+    }
+
+    /**
+     * This method is used to get the property value for the given key.
+     * It returns the given defaultKey if the property is not found.
+     *
+     * @param key This is the key to look up in the properties.
+     * @param defaultKey This is the String that will be returned if `key` cannot be found.
+     * @return String This returns the property value for the given key or the given `defaultKey` value.
+     */
+    public static String get(String key, String defaultKey) {
+        return properties.getProperty(key, defaultKey);
     }
 }
